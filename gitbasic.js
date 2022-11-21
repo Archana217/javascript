c1f77d0cdce2fd27455b674b819261f236b0f83f
Archana217 <archana21nimonkar@gmail.com>
2022-11-21 12:39:44 +0530

    add newlyw changes

--- gitbasic.js
+++ gitbasic.js
@@ -10,8 +10,20 @@
 
 
 //important commands 
+// //initially try to push file but error comes
+// ! [rejected]        main -> main (fetch first)
+// error: failed to push some refs to 'https://github.com/Archana217/gitinfo.git'
+// hint: Updates were rejected because the remote contains work that you do
+// hint: not have locally. This is usually caused by another repository pushing
+// hint: to the same ref. You may want to first integrate the remote changes
+// hint: (e.g., 'git pull ...') before pushing again.
+// hint: See the 'Note about fast-forwards' in 'git push --help' for details.
 
+
+// so follow this steps to overcome
 // Steps
+
+
 // 1) git init
 
 // 2) git status (for checking status)
@@ -29,8 +41,17 @@
 
 // pallavi is clever girl
 
+// how to add single file
+
+// git add filename
+// git commit -m "<pass your comment>"
+//git remote add origin "<pass your project clone url>"
+//git push -u "<pass your project clone url>" main
 
 
+
+//To pull file from git
+//git pull --allow-unrelated-histories "<pass your project clone url>" master
 
 
 
